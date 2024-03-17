b9ad948a22cae202712041ee072df234bc6166a8
TissueNam <39121270+TissueNam@users.noreply.github.com>
2024-03-17 23:30:37 +0900

    history(), charge(), use() 를 간단하게 채움 - 이게 맞나 싶기도... 테스트먼저 진행해야 할 느낌.

--- hhplus-tdd-jvm-java/src/main/java/io/hhplus/tdd/point/PointController.java
+++ hhplus-tdd-jvm-java/src/main/java/io/hhplus/tdd/point/PointController.java
@@ -1,5 +1,6 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,9 +15,15 @@ import java.util.List;
 @RestController
 public class PointController {
     private final UserPointTable userPointTable;
+    private final PointHistoryTable pointHistoryTable;
+
     @Autowired
-    public PointController(UserPointTable userPointTable){
+    public PointController(
+            UserPointTable userPointTable,
+            PointHistoryTable pointHistoryTable
+    ){
         this.userPointTable = userPointTable;
+        this.pointHistoryTable = pointHistoryTable;
     }
 
     /**
@@ -26,7 +33,7 @@ public class PointController {
     public UserPoint point(@PathVariable Long id)  {
         try {
             return userPointTable.selectById(id);
-        } catch (InterruptedException e) {
+        } catch (Exception e) {
             log.error("Error retrieving user point for id: {}", id, e);
             return new UserPoint(id, 0L, System.currentTimeMillis());
         }
@@ -37,22 +44,31 @@ public class PointController {
      */
     @GetMapping("{id}/histories")
     public List<PointHistory> history(@PathVariable Long id) {
-        return Collections.emptyList();
+//        return Collections.emptyList();
+        try {
+            return pointHistoryTable.selectAllByUserId(id);
+        } catch (Exception e) {
+            log.error("Error retrieving user's point history for id: {}", id, e);
+            return Collections.emptyList();
+        }
     }
 
     /**
      * TODO - 특정 유저의 포인트를 충전하는 기능을 작성해주세요.
      */
     @PatchMapping("{id}/charge")
-    public UserPoint charge(@PathVariable Long id, @RequestBody Long amount) {
-        return new UserPoint(0L, 0L, 0L);
+    public UserPoint charge(@PathVariable Long id, @RequestBody Long amount) throws InterruptedException{
+//        return new UserPoint(0L, 0L, 0L);
+        return userPointTable.insertOrUpdate(id, amount);
     }
 
     /**
      * TODO - 특정 유저의 포인트를 사용하는 기능을 작성해주세요.
      */
     @PatchMapping("{id}/use")
-    public UserPoint use(@PathVariable Long id, @RequestBody Long amount) {
-        return new UserPoint(0L, 0L, 0L);
+    public UserPoint use(@PathVariable Long id, @RequestBody Long amount) throws InterruptedException{
+//        return new UserPoint(0L, 0L, 0L);
+        pointHistoryTable.insert(id, amount, TransactionType.USE, 123L);
+        return userPointTable.selectById(id);
     }
 }
